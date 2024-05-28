18713df6930ada94b0dabcda0324e54706506ca2
codonssimplement <codons.simplement@gmail.com>
2024-05-28 14:07:55 +0200

    CREATE HOME PAGE 3F-GS

---
+++ Pages/HomePage/home.js
@@ -0,0 +1,17 @@
+// home.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginBtn").addEventListener("click", function() {
+        window.location.href = "../LoginPage/login.html"; 
+    });
+
+    document.getElementById("manageFoldersBtn").addEventListener("click", function() {
+        window.location.href = "../LoginPage/login.html"; 
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Hide loading screen after 2 seconds
+    setTimeout(function() {
+        document.getElementById("loading-screen").style.display = "none";
+    }, 750);
+});
